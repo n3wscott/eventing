e2c6dad85e867a4ffc24a466c954d26641d8dad0
Scott Nichols <nicholss@google.com>
2018-08-01 15:37:26 -0700

    Adding channel defaults and validation.

---
+++ pkg/apis/channels/v1alpha1/channel_validation.go
@@ -0,0 +1,63 @@
+/*
+Copyright 2018 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/knative/pkg/apis"
+)
+
+func (c *Channel) Validate() *apis.FieldError {
+	return c.Spec.Validate().ViaField("spec")
+}
+
+func (cs *ChannelSpec) Validate() *apis.FieldError {
+	refsBus := len(cs.Bus) != 0
+	refsClusterBus := len(cs.ClusterBus) != 0
+	if (!refsBus && !refsClusterBus) || (refsBus && refsClusterBus) {
+		return ErrMutuallyExclusiveFields("bus", "clusterBus")
+	}
+	return nil
+}
+
+func (current *Channel) CheckImmutableFields(og apis.Immutable) *apis.FieldError {
+	original, ok := og.(*Channel)
+	if !ok {
+		return &apis.FieldError{Message: "The provided original was not a Channel"}
+	}
+
+	ignoreArguments := cmpopts.IgnoreFields(ChannelSpec{}, "Arguments")
+	if diff := cmp.Diff(original.Spec, current.Spec, ignoreArguments); diff != "" {
+		return &apis.FieldError{
+			Message: "Immutable fields changed (-old +new)",
+			Paths:   []string{"spec"},
+			Details: diff,
+		}
+	}
+	return nil
+}
+
+// Boneyard
+
+// TODO: use from pkg when https://github.com/knative/pkg/pull/34 lands
+func ErrMutuallyExclusiveFields(fieldPaths ...string) *apis.FieldError {
+	return &apis.FieldError{
+		Message: "mutually exclusive fields, must set only one of",
+		Paths:   fieldPaths,
+	}
+}
